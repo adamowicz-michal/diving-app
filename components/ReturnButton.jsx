c33945634cb47823a94dd294c9f6b0643ac66800
michal.adamowicz <michal.adamowicz@coditive.com>
2022-11-14 15:05:52 +0100

    Add Return Button

---
+++ components/ReturnButton.jsx
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+
+function ReturnButton({ onPress }) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={styles.buttonInnerContainer}
+        onPress={onPress}
+        android_ripple={{ color: '#5f35ac' }}
+      >
+        <Text style={styles.buttonText}>X</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    alignSelf: 'flex-start',
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+  buttonInnerContainer: {},
+  buttonText: {
+    color: '#000',
+    fontSize: 20,
+  },
+});
+
+export default ReturnButton;
--- screens/CalculateMODScreen.jsx
+++ screens/CalculateMODScreen.jsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Button, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import ReturnButton from '../components/ReturnButton';
 
 function CalculateMODScreen({ showMainScreen }) {
   return (
-    <View>
-      <Button title="Go Back" onPress={showMainScreen} />
+    <View style={styles.container}>
+      <ReturnButton onPress={showMainScreen} />
       <Text>Calculate Modn Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    marginTop: 40,
+    paddingTop: 10,
+    paddingHorizontal: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+  },
+});
 
 export default CalculateMODScreen;
