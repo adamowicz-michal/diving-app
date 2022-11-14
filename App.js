ce5186b0984b61dd711c6dc330ffcacf23e73ab4
michal.adamowicz <michal.adamowicz@coditive.com>
2022-11-14 13:00:59 +0100

    Add backgroun image and styles

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <>
+    <ImageBackground
+      source={require('./assets/images/background.png')}
+      resizeMode="cover"
+      style={styles.imageContainer}
+      imageStyle={styles.backgroundImage}
+    >
       <StatusBar style="light" />
       <View style={styles.container}>
         <Text>Diving App</Text>
       </View>
-    </>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  backgroundImage: {
+    opacity: 0.75,
+  }
 });
